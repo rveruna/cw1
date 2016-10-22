8663739984fbbe9107833da5e8a348687c4445a9
rveruna <verunar@seznam.cz>
2016-10-22 11:21:29 +0000

    adding titles

--- src/index.py
+++ src/index.py
@@ -3,7 +3,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return render_template('index.html')
+  films = ['Comedies', 'Drama', 'Animation', 'Sci-fi']
+  return render_template('index.html', title='Home', films=films),200
+
+@app.route('/Comedies/')
+def Comedies():
+  return render_template('comedies.html')
+
+@app.route('/Drama/')
+def Drama():
+  return render_template('drama.html')
+
+@app.route('/Animation/')
+def Animation():
+  return render_template('animation.html')
+
+@app.route('/Sci-fi/')
+def Scifi():
+  return render_template('scifi.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
