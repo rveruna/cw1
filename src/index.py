66995c6fbad8653846e4afb689f2fc679ec490ac
veronika <verunar@seznam.cz>
2016-10-23 02:06:10 +0100

    favicon test

--- src/index.py
+++ src/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
 @app.route("/")
@@ -22,8 +22,9 @@ def Animation():
 def Scifi():
   return render_template('scifi.html', title='Animation')
 
-app.add_url_rule('/favicon.ico',
-                 redirect_to=url_for('static', filename='favicon.ico'))
-                 
+@app.route('/favicon.ico')
+def favicon():
+  return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
