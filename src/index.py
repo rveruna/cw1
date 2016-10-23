ca53f12fa1c989399a0d018aa0b6107c44cc0223
veronika <verunar@seznam.cz>
2016-10-23 02:38:36 +0100

    adding error page

--- src/index.py
+++ src/index.py
@@ -24,7 +24,7 @@ def Scifi():
 
 @app . errorhandler (404)
 def page_not_found ( error ):
-  return " Couldn't find the page you requested .", 404
+  return render_template('404.html', title='Error'), 404
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
