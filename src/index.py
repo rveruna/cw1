bd8ac1a4739b1e60c2bf003088059dc2e9cd7bcc
veronika <verunar@seznam.cz>
2016-10-23 02:31:33 +0100

    footer not working

--- src/index.py
+++ src/index.py
@@ -22,5 +22,9 @@ def Animation():
 def Scifi():
   return render_template('scifi.html', title='Animation')
 
+@app . errorhandler (404)
+def page_not_found ( error ):
+  return " Couldn't find the page you requested .", 404
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
