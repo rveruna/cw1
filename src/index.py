279f9681cdbf0c3db6e4c70a16dc60a0e4e435cb
rveruna <verunar@seznam.cz>
2016-10-24 19:51:16 +0000

    adding working json

--- src/index.py
+++ src/index.py
@@ -10,28 +10,33 @@ def showjson():
     data = json.load(open(json_url))
     return render_template('films.html', data=data)
 
-
+#route for homepage
 @app.route("/")
 def index():
   films = ['Comedies', 'Drama', 'Animation', 'Sci-fi']
   return render_template('index.html', title='Home', films=films),200
 
+#route for comedies page
 @app.route('/Comedies/')
 def Comedies():
   return render_template('comedies.html', title='Comedies')
 
+#route for Drama page
 @app.route('/Drama/')
 def Drama():
   return render_template('drama.html', title='Drama')
 
+#route for animation page
 @app.route('/Animation/')
 def Animation():
   return render_template('animation.html', title='Animation')
 
+#route for sci-fi page
 @app.route('/Sci-fi/')
 def Scifi():
   return render_template('scifi.html', title='Animation')
 
+#route for personalised error page
 @app.errorhandler (404)
 def page_not_found (error):
   return render_template('404.html', title='Error'), 404
