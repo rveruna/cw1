fd6177a0c7e909ae7349a178d8493201b235fdf2
veronika <verunar@seznam.cz>
2016-10-25 20:50:39 +0100

    adding json to scifi.html

--- src/index.py
+++ src/index.py
@@ -3,13 +3,12 @@ from flask import Flask, url_for, json, render_template
 app = Flask(__name__)
 
 # loading json for films page
-
 @app.route('/Films')
 def films():
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, "static/js/", "films.json")
-    data = json.load(open(json_url))
-    return render_template('films.html', data=data)
+  SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+  json_url = os.path.join(SITE_ROOT, "static/js/", "films.json")
+  data = json.load(open(json_url))
+  return render_template('films.html', data=data)
 
 #route for homepage
 @app.route("/")
@@ -20,31 +19,34 @@ def index():
 #loading json for comedies page
 @app.route('/Comedies')
 def comedies():
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, "static/js/", "films.json")
-    comedies = json.load(open(json_url))
-    return render_template('comedies.html', comedies=comedies)
+  SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+  json_url = os.path.join(SITE_ROOT, "static/js/", "films.json")
+  comedies = json.load(open(json_url))
+  return render_template('comedies.html', comedies=comedies)
 
-#loading json for Drama page
+#loading json for drama page
 @app.route('/Drama/')
 def drama():
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, "static/js/", "films.json")
-    drama = json.load(open(json_url))
-    return render_template('drama.html', drama=drama)
+  SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+  json_url = os.path.join(SITE_ROOT, "static/js/", "films.json")
+  drama = json.load(open(json_url))
+  return render_template('drama.html', drama=drama)
 
-#route for animation page
+#loading json for animation page
 @app.route('/Animation/')
 def animation():
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, "static/js/", "films.json")
-    animation = json.load(open(json_url))
-    return render_template('animation.html', animation=animation)
+  SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+  json_url = os.path.join(SITE_ROOT, "static/js/", "films.json")
+  animation = json.load(open(json_url))
+  return render_template('animation.html', animation=animation)
 
-#route for sci-fi page
+#loading json for sci-fi page
 @app.route('/Sci-fi/')
-def Scifi():
-  return render_template('scifi.html', title='Animation')
+def scifi():
+  SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+  json_url = os.path.join(SITE_ROOT, "static/js/", "films.json")
+  scifi = json.load(open(json_url))
+  return render_template('scifi.html', scifi=scifi)
 
 #route for personalised error page
 @app.errorhandler (404)
